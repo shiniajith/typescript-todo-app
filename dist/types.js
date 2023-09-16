5ceb3acf910393918ec92eb64dc725a8e79754fe
Shini Ajithkumar <sajith511@gmail.com>
2023-09-16 21:03:20 +0100

    Updates

--- dist/types.js
+++
@@ -1,6 +0,0 @@
-export var TodoListState;
-(function(TodoListState2) {
-  TodoListState2[TodoListState2["ALL"] = 0] = "ALL";
-  TodoListState2[TodoListState2["ACTIVE"] = 1] = "ACTIVE";
-  TodoListState2[TodoListState2["COMPLETED"] = 2] = "COMPLETED";
-})(TodoListState || (TodoListState = {}));
